398c51be6546f76eb3522c40efebe43565154eef
reza <rezamoaddab1998@gmail.com>
2025-09-23 01:17:30 +0330

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let signer;
 
 // ZenChain Testnet details
 const ZENCHAIN_PARAMS = {
-  chainId: "0x20E8", // 8408 in hex
+  chainId: "0x20d8", // 8408 in hex
   chainName: "ZenChain Testnet",
   nativeCurrency: {
     name: "ZenChain Test Coin",
